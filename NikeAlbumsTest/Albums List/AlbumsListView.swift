293ad2174ca1a2b4e898f0b31a458c37eff330e2
Nate Potter <nathanielpotter@gmail.com>
2021-01-11 20:54:20 -0800

    Add AlbumsListViewController and View

---
+++ NikeAlbumsTest/Albums List/AlbumsListView.swift
@@ -0,0 +1,49 @@
+//
+//  AlbumsListView.swift
+//  NikeAlbumsTest
+//
+//  Created by Nate Potter on 1/11/21.
+//
+
+import UIKit
+
+class AlbumListView: UIView {
+    private let collectionViewLayout: UICollectionViewLayout = {
+        var config = UICollectionLayoutListConfiguration(appearance: .plain)
+        let layout = UICollectionViewCompositionalLayout.list(using: config)
+        
+        return layout
+    }()
+    
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return collectionView
+    }()
+    
+    lazy var refreshControl : UIRefreshControl = {
+        var refreshControl = UIRefreshControl()
+        refreshControl.tintColor = UIColor.gray
+        return refreshControl
+    }()
+    
+    let padding: CGFloat = 20
+    
+    convenience init() {
+        self.init(frame: .zero)
+        
+        backgroundColor = .systemBackground
+        
+        directionalLayoutMargins = NSDirectionalEdgeInsets(top: padding, leading: padding, bottom: padding, trailing: padding)
+                
+        addSubview(collectionView)
+
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor)
+        ])
+    }
+}
---
+++ NikeAlbumsTest/Albums List/AlbumsListViewController.swift
@@ -0,0 +1,112 @@
+//
+//  AlbumsListViewController.swift
+//  NikeAlbumsTest
+//
+//  Created by Nate Potter on 1/11/21.
+//
+
+import UIKit
+import Combine
+
+enum Section: CaseIterable {
+    case main
+}
+
+class AlbumsListViewController: UIViewController {
+    
+    let customView = AlbumListView()
+    
+    let interactor: AlbumsListViewInteractor
+                
+    var viewModel: AlbumsModels.ViewModel?
+    
+    lazy var cellConfig: UICollectionView.CellRegistration<UICollectionViewListCell, AlbumsModels.DisplayAlbum> = {
+        UICollectionView.CellRegistration<UICollectionViewListCell, AlbumsModels.DisplayAlbum> { cell, indexPath, model in
+            var config = AlbumCellContentConfiguration()
+            config.name = model.name
+            config.artist = model.artist
+            config.image = model.thumbnail
+            
+            if model.thumbnail == nil {
+                config.cancellable = ImageLoader.shared.loadImage(path: model.albumArtworkUrl)
+                .catch{ error in return Just(nil) }
+                .sink(receiveValue: { image in
+                    if let album = self.dataSource.itemIdentifier(for: indexPath) {
+                        var updatedModel = album
+                        updatedModel.thumbnail = image
+                        var newSnapshot = self.dataSource.snapshot()
+                        newSnapshot.insertItems([updatedModel], beforeItem: album)
+                        newSnapshot.deleteItems([album])
+                        self.dataSource.apply(newSnapshot, animatingDifferences: false)
+                    }
+                })
+            }
+            
+            cell.accessories = [.disclosureIndicator()]
+            
+            cell.contentConfiguration = config
+        }
+    }()
+    
+    lazy var dataSource = {
+        UICollectionViewDiffableDataSource<Section, AlbumsModels.DisplayAlbum>(collectionView: customView.collectionView) { collectionView, indexPath, item -> UICollectionViewCell? in
+            collectionView.dequeueConfiguredReusableCell(using: self.cellConfig, for: indexPath, item: item)
+        }
+    }()
+    
+    override func loadView() {
+        view = customView
+    }
+    
+    init() {
+        interactor = AlbumsListViewInteractor(library: Library(client: Client()))
+        
+        super.init(nibName: nil, bundle: nil)
+        
+        interactor.presenter = AlbumsListViewPresenter(viewController: self)
+
+        navigationItem.title = NSLocalizedString("albums.list.title", comment: "ViewController Title")
+        
+        customView.collectionView.delegate = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        customView.collectionView.dataSource = dataSource
+        
+        // with more time I would construct the path with path componenet parameters
+        interactor.fetch(request: AlbumsModels.Request(url: "/us/apple-music/top-albums/all/100/explicit.json"))
+    }
+    
+    func display(viewModel: AlbumsModels.ViewModel) {
+        self.viewModel = viewModel
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, AlbumsModels.DisplayAlbum>()
+        snapshot.appendSections(Section.allCases)
+        
+        snapshot.appendItems(viewModel.albums, toSection: .main)
+        
+        dataSource.apply(snapshot)
+    }
+    
+    func display(error: Error) {
+        let alertController = UIAlertController(title: "Whoooops!", message: error.localizedDescription, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
+}
+
+extension AlbumsListViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let album = dataSource.itemIdentifier(for: indexPath) {
+            let viewController = AlbumDetailViewController(album: album)
+            navigationController?.pushViewController(viewController, animated: true)
+        }
+    }
+}
